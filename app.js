c637d1ac51441f889cda450990eec3f5667bb329
BUMJH0121 <bumjh0121@gmail.com>
2021-02-03 16:59:41 +0900

    second

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000
 
+app.get('/', (req,res) => res.send('Welcome'));
 app.get('/service', (req,res) => {
     var status = req.param('status')
     res.send('The system is running ['+ status + ']');
