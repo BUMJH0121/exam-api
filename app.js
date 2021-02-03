8786e70703c07444f7a89cd5a3d4385e0068b9af
BUMJH0121 <bumjh0121@gmail.com>
2021-02-03 16:56:29 +0900

    exam

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/service', (req,res) => {
+    var status = req.param('status')
+    res.send('The system is running ['+ status + ']');
+});
+app.listen(port,() => console.log('Example app listening onport 3000'))
